5420a8dfa03da283d0699cc5651f215fb1a8461d
LucasCid <lucascidcanete@gmail.com>
2025-08-25 14:08:58 -0400

    resolviendo deploy

---
+++ config/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:3000',
+        'https://aromascafe2.vercel.app',
+        'https://*.vercel.app',
+        /\.vercel\.app$/,
+      ]
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
